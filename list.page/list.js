39baad90aab3dec0931a3ba1cd7606312455927f
TamerlanAlliyev <tamerlanma@code.edu.az>
2024-02-07 21:48:07 +0400

    home page finshed

--- list.page/list.js
+++ list.page/list.js
@@ -1,7 +1,4 @@
 const userList = localStorage.getItem('userList').split(',');
-
-console.log(userList);
-
 const list = document.querySelector('.list ul');
 
 userList.forEach(value => {
@@ -13,16 +10,19 @@ userList.forEach(value => {
                 <img src="../assets/icons/userprofil.svg" alt="">
                 <a target="_blank" href="https://www.instagram.com/${value}/">${value}</a>
             </div>
-            <img class="cricle" src="../assets/icons/cricle.svg" alt="">
+            <img class="circle" src="../assets/icons/cricle.svg" alt="">
         </li>
     `;
+});
 
-    const listElement = document.querySelectorAll('.user-info a'); 
-    const circle = document.querySelectorAll('.cricle'); 
+const listElements = document.querySelectorAll('.list ul li');
+const circle = document.querySelectorAll('.circle');
+const userName = document.querySelectorAll('.user-info a');
 
-    listElement.forEach((element, index) => {
-        element.addEventListener('click', () => {
-            circle[index].src = '../assets/icons/checkbox.svg';
-        });
+listElements.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        const link = element.querySelector('a').href;
+        circle[index].src = '../assets/icons/checkbox.svg';
+        window.open(link, '_blank');
     });
 });
