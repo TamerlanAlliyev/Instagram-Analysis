ea4492d1d7c57136c237b00db9015f7b971b0264
TamerlanAlliyev <tamerlanma@code.edu.az>
2024-02-07 23:12:22 +0400

    list responsive finshed

--- list.page/list.js
+++ list.page/list.js
@@ -26,3 +26,22 @@ listElements.forEach((element, index) => {
         window.open(link, '_blank');
     });
 });
+
+
+const categoryIcon = document.querySelector('.category img');
+const categoryList = document.querySelector('.category ul');
+
+let toogle = false;
+categoryIcon.addEventListener('click', () => {
+
+    if(!toogle){
+        categoryIcon.src="../assets/icons/Close-icon.svg"
+        categoryList.style.display = "flex";
+        toogle = true;
+    }else{
+        categoryIcon.src="../assets/icons/Category.svg"
+        categoryList.style.display = "none";
+        toogle = false;
+    }
+    
+});
