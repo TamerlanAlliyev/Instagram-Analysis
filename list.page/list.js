33d3e38995025c5df4d7d006168d1ee5cdb06757
TamerlanAlliyev <tamerlanma@code.edu.az>
2024-02-08 18:25:35 +0400

    navbar update

--- list.page/list.js
+++ list.page/list.js
@@ -30,17 +30,19 @@ listElements.forEach((element, index) => {
 
 const categoryIcon = document.querySelector('.category img');
 const categoryList = document.querySelector('.category ul');
-
+const mainHead = document.querySelector('main .head');
 let toogle = false;
 categoryIcon.addEventListener('click', () => {
 
     if(!toogle){
         categoryIcon.src="../assets/icons/Close-icon.svg"
         categoryList.style.display = "flex";
+        mainHead.style.marginTop = "95px";
         toogle = true;
     }else{
         categoryIcon.src="../assets/icons/Category.svg"
         categoryList.style.display = "none";
+        mainHead.style.marginTop = "inherit";
         toogle = false;
     }
     
